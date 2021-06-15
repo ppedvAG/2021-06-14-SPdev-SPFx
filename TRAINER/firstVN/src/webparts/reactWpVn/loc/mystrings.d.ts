34e8ad99c34811e97f5676a6b44686d278ae9497
vadzim3402 <vadzim@naumchyk.onmicrosoft.com>
2021-06-15 13:08:55 +0200

    react input from proppane

---
+++ TRAINER/firstVN/src/webparts/reactWpVn/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactWpVnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactWpVnWebPartStrings' {
+  const strings: IReactWpVnWebPartStrings;
+  export = strings;
+}
